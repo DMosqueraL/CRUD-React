5470a68b06c87cfc0a08eb093b57d5ca70247a97
DMosqueral <dml2410@gmail.com>
2022-05-08 13:36:38 -0500

    DELETE --- Se borran archivos innecesarios en el proyecto

--- CRUD-React/src/index.js
+++ CRUD-React/src/index.js
@@ -7,4 +7,9 @@ import App from './App.js';
 /** Sentencia en desuso en React18
  * ReactDOM.render(<App />, document.getElementById('root'));**/
 
- ReactDOM.createRoot(<App />,document.getElementById('root'));
+ const root = ReactDOM.createRoot(document.getElementById('root'));
+ root.render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>
+ );
